556ee9888881108b23eab17227dab668942084db
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-07-24 20:28:59 +0300

    add emoji resources

---
+++ src/main/java/com/ohrim/constants/EmojiConstants.java
@@ -0,0 +1,9 @@
+package com.ohrim.constants;
+
+public class EmojiConstants {
+    public static final String TREE = "\uD83C\uDF33";
+    public static final String ROCK = "\uD83D\uDDFF" ;
+    public static final String GROUND = "\uD83D\uDFE4 " ;
+    public static final String GRASS = "\uD83C\uDF3F " ;
+
+}
