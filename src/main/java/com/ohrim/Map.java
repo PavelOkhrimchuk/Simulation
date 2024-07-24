c891222ff4320018f4dfdc69f68f626600f021b0
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-07-24 22:16:54 +0300

    Map first implementation

--- src/main/java/com/ohrim/Map.java
+++ src/main/java/com/ohrim/Map.java
@@ -1,4 +1,49 @@
 package com.ohrim;
 
+import com.ohrim.entities.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Map {
+    private int width;
+    private int height;
+
+    private Entity[][] field;
+
+    public Map(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.field = new Entity[width][height];
+
+    }
+
+    public Map() {
+        this(30, 30);
+    }
+
+    public void addEntity(Entity entity) {
+        Coordinates coordinates = entity.getCoordinates();
+        if (isWithinBounds(coordinates) && field[coordinates.getX()][coordinates.getY()] == null) {
+            field[coordinates.getX()][coordinates.getY()] = entity;
+        } else {
+            System.out.println("Invalid position or cell already occupied");
+        }
+    }
+
+    public void updateField() {
+        for (Entity[] entities : field) {
+            for (Entity entity : entities) {
+                field[entity.getCoordinates().getX()][entity.getCoordinates().getY()] = entity;
+            }
+        }
+    }
+
+    private boolean isWithinBounds(Coordinates coordinates) {
+        return coordinates.getX() >= 0 && coordinates.getX() < width
+                && coordinates.getY() >= 0 && coordinates.getY() < height;
+    }
+
+
 }
