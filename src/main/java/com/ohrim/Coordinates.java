2bcafe5533e30a1c7308855b18c4b5bfdc8b2808
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-07-25 12:52:56 +0300

    override equals and hashCode

--- src/main/java/com/ohrim/Coordinates.java
+++ src/main/java/com/ohrim/Coordinates.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -13,7 +15,16 @@ public class Coordinates {
     private int x;
     private int y;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return x == that.x && y == that.y;
+    }
 
-
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
