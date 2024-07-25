41b26b3f0bc7a6cc162968057ed4f8c1a09de2c5
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-07-25 19:32:57 +0300

    launch class implementation

--- src/main/java/com/ohrim/Simulation.java
+++ src/main/java/com/ohrim/Simulation.java
@@ -3,29 +3,40 @@ package com.ohrim;
 import com.ohrim.actions.MapInitializer;
 import com.ohrim.actions.MoveAction;
 import lombok.Getter;
+import lombok.Setter;
+
+import java.util.concurrent.TimeUnit;
 
 @Getter
+@Setter
 public class Simulation {
 
 
+    private final MapInitializer initAction = new MapInitializer();
+    private final MoveAction turnAction = new MoveAction();
     private final Renderer renderer = new Renderer();
-
-    private final MapInitializer mapInitializer = new MapInitializer();
-    private final MoveAction moveAction = new MoveAction();
-    private int moveCounter = 0;
     private Map map;
-
+    private boolean isGameStopped = false;
 
     public void nextTurn() {
+        turnAction.executeCreatureMoves(map);
+        turnAction.renderMap(map);
+        System.out.println(" ");
 
     }
-    public void startSimulation() {
 
+    public void startSimulation() throws InterruptedException {
+        map = initAction.initializeMap();
+
+        while (!isGameStopped) {
+            nextTurn();
+            TimeUnit.SECONDS.sleep(2);
+        }
     }
 
-    public void pauseSimulation() {
 
-    }
+
+
 
 
 }
