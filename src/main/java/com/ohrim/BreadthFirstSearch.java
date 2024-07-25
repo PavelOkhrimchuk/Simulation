6bca3dd14f4b0d36029a631d86cfc157bf33ef7d
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-07-25 11:57:36 +0300

    declaration of methods to implement the algorithm

--- src/main/java/com/ohrim/BreadthFirstSearch.java
+++ src/main/java/com/ohrim/BreadthFirstSearch.java
@@ -1,4 +1,42 @@
 package com.ohrim;
 
+import com.ohrim.entities.Entity;
+
+import java.util.*;
+
 public class BreadthFirstSearch {
+    private final Simulation simulation = AppRunner.simulation;
+    private final List<Coordinates> queuedCoordinates = new ArrayList<>();
+    private final List<Coordinates> exploredCoordinates = new ArrayList<>();
+    private final HashMap<Coordinates, Coordinates> childParentMap = new HashMap<>();
+    private Entity[][] field;
+    private Entity targetEntity;
+
+    public List<Coordinates> findClosestObjectCoordinates(Coordinates currentCoordinates, Entity targetEntity) {
+
+    }
+
+    private Optional<Coordinates> exploreQueuedCoordinates() {
+
+
+    }
+
+
+    private void enqueueNeighbors() {
+    }
+
+    private void enqueueNeighbors(Coordinates coordinates) {
+
+    }
+
+
+
+
+    private List<Coordinates> findPathToTarget(Coordinates targetCoordinates) {
+
+    }
+
+
+
+
 }
