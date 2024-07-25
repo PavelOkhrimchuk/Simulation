6da5c788fe0ea40b1cd0501860ebb5c335e648e9
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-07-25 16:35:35 +0300

    Creating getters and setters for hp and speed

--- src/main/java/com/ohrim/entities/creatures/Creature.java
+++ src/main/java/com/ohrim/entities/creatures/Creature.java
@@ -1,7 +1,11 @@
 package com.ohrim.entities.creatures;
 
 import com.ohrim.entities.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public abstract class Creature extends Entity {
 
     int hp = 5;
