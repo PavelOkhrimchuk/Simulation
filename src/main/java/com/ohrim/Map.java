83267413341ef008fbcbb352406405294a1a017d
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-07-26 01:30:40 +0300

    Refactor Map class to improve encapsulation and interface

--- src/main/java/com/ohrim/Map.java
+++ src/main/java/com/ohrim/Map.java
@@ -4,41 +4,46 @@ import com.ohrim.entities.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashMap;
+
 @Getter
 @Setter
 public class Map {
     private int width;
     private int height;
-
-    private Entity[][] field;
+    private HashMap<Coordinates, Entity> cells;
 
     public Map(int width, int height) {
         this.width = width;
         this.height = height;
-        this.field = new Entity[width][height];
-
+        this.cells = new HashMap<>();
     }
 
     public Map() {
         this(5, 5);
     }
 
-
-
-
-
-    public void addEntity(Entity entity) {
-        Coordinates coordinates = entity.getCoordinates();
-        field[coordinates.getX()][coordinates.getY()] = entity;
-
+    public void placeEntity(Entity entity, Coordinates coordinates) {
+        entity.setCoordinates(coordinates);
+        cells.put(coordinates, entity);
     }
 
-    public void updateField() {
-        for (Entity[] entities : field) {
-            for (Entity entity : entities) {
-                field[entity.getCoordinates().getX()][entity.getCoordinates().getY()] = entity;
-            }
-        }
+    public Entity getEntity(Coordinates coordinates) {
+        return cells.get(coordinates);
+    }
+
+    public boolean isCellEmpty(Coordinates coordinates) {
+        return !cells.containsKey(coordinates);
+    }
+
+    public void removeEntity(Coordinates coordinates) {
+        cells.remove(coordinates);
+    }
+
+    public void moveEntity(Coordinates from, Coordinates to) {
+        Entity entity = getEntity(from);
+        removeEntity(from);
+        placeEntity(entity, to);
     }
 
 
