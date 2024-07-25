166382a08523dc790bcd6aad4334823f1232fc54
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-07-25 16:49:49 +0300

    Renderer method implementation

--- src/main/java/com/ohrim/Renderer.java
+++ src/main/java/com/ohrim/Renderer.java
@@ -1,4 +1,17 @@
 package com.ohrim;
 
+import com.ohrim.entities.Entity;
+
 public class Renderer {
+    public void render(Map mapObj) {
+        Entity[][] map = mapObj.getField();
+        for (Entity[] entityRow : map) {
+            for (Entity entity : entityRow) {
+                System.out.print(entity.toString() + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
 }
